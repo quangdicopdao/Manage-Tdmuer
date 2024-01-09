032023c99f1d88df18c730c021e3596084954b17
quangdicopdao <quangdv.12c8bc.1920@gmail.com>
2024-01-09 20:23:10 +0700

    done giao dien

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -5,44 +5,101 @@ import moment from 'moment';
 import classNames from 'classnames/bind';
 import style from './Calendar.module.scss';
 import Modal from '../Modal/Modal';
+import Button from '../Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-regular-svg-icons';
 
 const cx = classNames.bind(style);
 const localizer = momentLocalizer(moment);
+const myEventsList = [
+    {
+        start: new Date(2024, 0, 9, 10, 0),
+        end: new Date(2024, 0, 9, 12, 0),
+        title: 'Meeting with Client',
+        color: 'red',
+    },
+    {
+        start: new Date(2024, 0, 10, 14, 30),
+        end: new Date(2024, 0, 10, 16, 0),
+        title: 'Team Workshop',
+        color: 'blue',
+    },
+];
 
-const MyCalendar = () => {
-    const [events, setEvents] = useState([]);
+// ...
 
+// ...
+
+function MyCalendar() {
+    const [modalOpen, setModalOpen] = useState(false);
     const [selectedSlot, setSelectedSlot] = useState(null);
-    const [showModal, setShowModal] = useState(false);
 
-    const handleSelectSlot = (slotInfo) => {
+    const openModal = (slotInfo) => {
         setSelectedSlot(slotInfo);
-        setShowModal(true);
+        setModalOpen(true);
     };
 
-    const handleHideModal = () => {
-        setShowModal(false);
-    };
-
-    const handleAddEvent = (newEvent) => {
-        setEvents([...events, newEvent]);
-        setShowModal(false);
+    const closeModal = () => {
+        setSelectedSlot(null);
+        setModalOpen(false);
     };
 
     return (
         <div className={cx('wrapper')}>
             <Calendar
                 localizer={localizer}
-                events={events}
+                events={myEventsList}
                 startAccessor="start"
                 endAccessor="end"
-                style={{ height: 500 }}
-                selectable
-                onSelectSlot={handleSelectSlot}
+                style={{ height: 550 }}
+                eventPropGetter={(event) => ({
+                    style: {
+                        backgroundColor: event.color,
+                    },
+                })}
+                onSelectEvent={() => openModal()}
+                onSelectSlot={(slotInfo) => openModal(slotInfo)}
+                selectable={true} // Enable selection of entire days
             />
-            {showModal && <Modal selectedSlot={selectedSlot} onClose={handleHideModal} onAddEvent={handleAddEvent} />}
+            {modalOpen && (
+                <Modal onClose={closeModal}>
+                    {/* Render modal content using selectedSlot */}
+                    <div className={cx('wrap-modal')}>
+                        <div className={cx('modal-header')}>
+                            <h2>Thêm sự kiện vào {selectedSlot && selectedSlot.start.toLocaleDateString()}</h2>
+                        </div>
+                        <div className={cx('modal-content')}>
+                            <div className={cx('wrap-input-modal')}>
+                                <input
+                                    type="text"
+                                    placeholder="Nhập tiêu đề cho sự kiện"
+                                    className={cx('input-modal')}
+                                />
+                            </div>
+                            <div className={cx('wrap-more')}>
+                                <div className={cx('modal-components')}>
+                                    <FontAwesomeIcon icon={faClock} className={cx('icon')} />
+                                    <h4 className={cx('title')}>Chọn giờ</h4>
+                                </div>
+
+                                <div className={cx('wrap-desciptiom')}>
+                                    <h4 className={cx('title')}>Mô tả</h4>
+                                    <textarea className={cx('desciption')}></textarea>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={cx('modal-footer')}>
+                            <Button primary className={cx('btn-footer')}>
+                                Lưu
+                            </Button>
+                        </div>
+                    </div>
+                </Modal>
+            )}
         </div>
     );
-};
+}
+
+// ...
 
 export default MyCalendar;
